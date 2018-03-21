adb1adae89a50eda2705027f67d31c268c1d1e1a
jitender singh <jitender@enacteservices.com>
2018-03-21 13:17:22 +0530

    added the staticfiles directory

--- autobetting/autobetting/settings.py
+++ autobetting/autobetting/settings.py
@@ -118,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# static settings
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
