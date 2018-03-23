5342175ce004e95992f22ca795c5641eb9d89d61
jitender singh <jitender@enacteservices.com>
2018-03-23 12:52:34 +0530

    Create the Dashboard view

--- autobetting/dashboard/views.py
+++ autobetting/dashboard/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views import generic
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+
+class DashboardView(generic.TemplateView):
+    """
+    View handle and show the dash-board
+    """
+    template_name = "dashboard/index.html"
+
+    @method_decorator(login_required(login_url=reverse_lazy('users:login')))
+    def dispatch(self, request, *args, **kwargs):
+        return super(DashboardView, self).dispatch(request, *args, **kwargs)
